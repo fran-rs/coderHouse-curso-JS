d0413c807085535c3459d9b3587bbbf2197bb091
fransilandia <mafca21@gmail.com>
2024-01-23 18:37:09 -0300

    clase 10

--- clase-10/main.js
+++ clase-10/main.js
@@ -17,9 +17,18 @@
 // let evento = prompt("agregue el evento")
 // boton.addEventListener(evento, saludar);
 
-const saludar = (nombre) => {
-     console.log(  `hola click ${nombre}` );
-};
+// const saludar = (nombre) => {
+//      console.log(  `hola click ${nombre}` );
+// };
 
-let nombre = prompt("ingrese el nombre")
-boton.addEventListener("click", () => saludar(nombre)); 
+// let nombre = prompt("ingrese el nombre")
+// boton.addEventListener("click", () => saludar(nombre)); 
+
+
+// EVENTOS MAS COMUNES
+
+let boton = document.getElementById("boton");
+// boton.addEventListener("mousedown" , () => console.log("mousedown"));
+// boton.addEventListener("mouseup" , () => console.log("mouseup"));
+// boton.addEventListener("mouseover" , () => console.log("mouseover"));
+boton.addEventListener("mouseout" , () => console.log("mouseout"));
